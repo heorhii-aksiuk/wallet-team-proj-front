1b94bc08b581d024c990e0ccb586991645369f0a
Vlad13b <vldbarsuk@gmail.com>
2022-05-12 10:51:58 +0300

    done table styles

---
+++ src/components/Dashbord/Dashbord.jsx
@@ -0,0 +1,13 @@
+
+import { Table, Thead, TrHead, Th, Tbody, Tr, Td, Col, Text } from './styled';
+
+const Statistic = () => {
+
+  return (
+    <>
+
+    </>
+  );
+};
+
+export default Statistic;
