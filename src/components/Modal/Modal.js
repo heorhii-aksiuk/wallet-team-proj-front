88203f55306998dddfd2aa51048a2e1c089fbac5
Vlad13b <vldbarsuk@gmail.com>
2022-05-12 14:29:03 +0300

    Notification

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useCallback } from 'react';
+import { createPortal } from 'react-dom';
+import s from './Modal.module.css';
+
+const modalRoot = document.querySelector('#modal-root');
+
+export default function Modal({ onClose, children }) {
+    useEffect(() => {
+        const handleKeyDown = e => {
+            if (e.code === 'Escape') {
+                onClose();
+            }
+        };
+
+        window.addEventListener('keydown', handleKeyDown);
+
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onClose]);
+
+    const handleBackdropClick = useCallback(
+        event => {
+            if (event.currentTarget === event.target) {
+                onClose();
+            }
+        }, [onClose],
+    );
+
+    return createPortal( <div className = { s.Modal__backdrop }onClick = { handleBackdropClick } >
+        <div className = { s.Modal__content } onClose = { onClose } > { children } </div> </div > ,
+        modalRoot,
+    );
+}
