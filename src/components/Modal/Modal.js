5f78dc4bd6aee22f9efc5ef07d026595ae03365f
Vlad13b <vldbarsuk@gmail.com>
2022-05-12 12:43:29 +0300

    modal-fix

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,39 +1,40 @@
 import React, { useEffect, useCallback } from 'react';
 import { createPortal } from 'react-dom';
-import './Modal.css';
+import s from './Modal.module.css';
 
 const modalRoot = document.querySelector('#modal-root');
 
 export default function Modal({ onClose, children }) {
-  useEffect(() => {
-    const handleKeyDown = e => {
-      if (e.code === 'Escape') {
-        onClose();
-      }
-    };
+    useEffect(() => {
+        const handleKeyDown = e => {
+            if (e.code === 'Escape') {
+                onClose();
+            }
+        };
 
-    window.addEventListener('keydown', handleKeyDown);
+        window.addEventListener('keydown', handleKeyDown);
 
-    return () => {
-      window.removeEventListener('keydown', handleKeyDown);
-    };
-  }, [onClose]);
+        return () => {
+            window.removeEventListener('keydown', handleKeyDown);
+        };
+    }, [onClose]);
 
-  const handleBackdropClick = useCallback(
-    event => {
-      if (event.currentTarget === event.target) {
-        onClose();
-      }
-    },
-    [onClose],
-  );
+    const handleBackdropClick = useCallback(
+        event => {
+            if (event.currentTarget === event.target) {
+                onClose();
+            }
+        }, [onClose],
+    );
 
-  return createPortal(
-    <div className="Modal__backdrop" onClick={handleBackdropClick}>
-      <div className="Modal__content" onClose={onClose}>
-        {children}
-      </div>
-    </div>,
-    modalRoot,
-  );
+    return createPortal( <
+        div className = { s.Modal__backdrop }
+        onClick = { handleBackdropClick } >
+        <
+        div className = { s.Modal__content }
+        onClose = { onClose } > { children } <
+        /div> <
+        /div>,
+        modalRoot,
+    );
 }
