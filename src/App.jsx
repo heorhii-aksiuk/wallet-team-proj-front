736d9a6415a6d12d2bcdb1d355021fba38005df1
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-05-13 18:57:14 +0300

    mobile mvp

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import Media from 'react-media'
 import { mediaQueries } from './utils/constants'
+import Header from './components/Header'
 
 function App() {
   return (
     <>
-      <Media queries={mediaQueries}>
+      {/* <Media queries={mediaQueries}>
         {(matches) =>
           matches.desktop ? <p>I am desktop!</p> : <p>I am not desktop!</p>
         }
-        
-      </Media>
+      </Media> */}
+      <Header></Header>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,31 @@
-// import s from './Header.module.css'
-// import sprite from '../../assets/svg/sprite.svg'
+import s from './Header.module.css'
+import sprite from '../../assets/svg/sprite.svg'
 
-// export default function Header({ media }) {
-//   return (
-//     <>
-//       {media === 'desktop' ? (
-//         <header>Header desktop</header>
-//       ) : (
-//         <header className={s.header}>
-//           <a href="./">
-//             <svg width="30" height="30">
-//               <use href={`${sprite}#icon-wallet`}></use>
-//             </svg>
-//             Wallet
-//           </a>
-//         </header>
-//       )}
-//     </>
-//   )
-// }
+export default function Header({ media }) {
+  return (
+    <>
+      {media === 'desktop' ? (
+        <header>Header desktop</header>
+      ) : (
+        <header className={s.header}>
+          <div className={s.logoContainer}>
+            <a href="./">
+              <svg width="30" height="30">
+                <use href={`${sprite}#icon-wallet`}></use>
+              </svg>
+            </a>
+            <a className={s.logo} href="./">
+              Wallet
+            </a>
+          </div>
+          <div className={s.authContainer}>
+            <p className={s.name}>Имя</p>
+            <svg className={s.svg} width="26" height="26">
+              <use href={`${sprite}#icon-exit`}></use>
+            </svg>
+          </div>
+        </header>
+      )}
+    </>
+  )
+}
