218bb922d61c09e6cb98358f0b66870e90012323
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-05-13 16:13:35 +0300

    some fix

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,21 @@
+// import s from './Header.module.css'
+// import sprite from '../../assets/svg/sprite.svg'
+
+// export default function Header({ media }) {
+//   return (
+//     <>
+//       {media === 'desktop' ? (
+//         <header>Header desktop</header>
+//       ) : (
+//         <header className={s.header}>
+//           <a href="./">
+//             <svg width="30" height="30">
+//               <use href={`${sprite}#icon-wallet`}></use>
+//             </svg>
+//             Wallet
+//           </a>
+//         </header>
+//       )}
+//     </>
+//   )
+// }
