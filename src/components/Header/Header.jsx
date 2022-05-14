231c36f62b3540acd28c4b203e3a284811ed214a
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-05-14 17:47:29 +0300

    Header c-t markup refactoring and sprite fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,49 +1,30 @@
 import s from './Header.module.css'
 import sprite from '../../assets/svg/sprite.svg'
 
-export default function Header({ media }) {
+export default function Header() {
   return (
     <>
-      {media === 'mobile' ? (
-        <header className={s.headerMobile}>
-          <div className={s.logoContainerMobile}>
-            <a href="./">
-              <svg width="30" height="30">
-                <use href={`${sprite}#icon-wallet`}></use>
-              </svg>
-            </a>
-            <a className={s.logoMobile} href="./">
-              Wallet
-            </a>
-          </div>
-          <div className={s.authContainerMobile}>
-            <p className={s.nameMobile}>Имя</p>
-            <svg width="26" height="26">
+      <header className={s.header}>
+        <div className={s.logoContainer}>
+          <a href="./">
+            <svg className={s.logoSvg}>
+              <use href={`${sprite}#icon-logo-full`}></use>
+            </svg>
+          </a>
+        </div>
+        <div className={s.authContainer}>
+          <p className={s.name}>Имя</p>
+          <svg className={s.lineSvg}>
+            <use href={`${sprite}#icon-vertical-line`}></use>
+          </svg>
+          <a href="./">
+            <svg className={s.exitSvg}>
               <use href={`${sprite}#icon-exit`}></use>
             </svg>
-          </div>
-        </header>
-      ) : (
-        <header className={s.header}>
-          <div className={s.logoContainer}>
-            <a href="./">
-              <svg width="40" height="40">
-                <use href={`${sprite}#icon-wallet`}></use>
-              </svg>
-            </a>
-            <a className={s.logo} href="./">
-              Wallet
-            </a>
-          </div>
-          <div className={s.authContainer}>
-            <p className={s.name}>Имя |</p>
-            <svg className={s.svg} width="26" height="26">
-              <use href={`${sprite}#icon-exit`}></use>
-            </svg>
-            <p className={s.name}>Выйти</p>
-          </div>
-        </header>
-      )}
+            <p className={s.exit}>Выйти</p>
+          </a>
+        </div>
+      </header>
     </>
   )
 }
