55b094bebc0a2988024e14d3098784c9d9dc98be
Vlad13b <vldbarsuk@gmail.com>
2022-05-15 14:16:26 +0300

    Bag-fix

--- src/components/ModalLogout/ModalLogout.jsx
+++ src/components/ModalLogout/ModalLogout.jsx
@@ -1,9 +1,9 @@
 import { useDispatch } from 'react-redux';
 import { useCallback } from 'react';
-import {closeModalTransaction} from "../../redux/transactions/transaction.actions";
+import {closeModalTransaction} from "../../redux/transactions/transaction-actions";
 import Modal from "../Modal";
 
-export default function closeModal() {
+export default function ModalLogout() {
     const dispatch = useDispatch()
     const closeTransaction = useCallback(() => {
         return dispatch(closeModalTransaction())
