e777183f8bc4c99d4b859f8058365cfc25535d7b
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2022-05-16 18:03:44 +0300

    add formik-form

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -1,31 +1,20 @@
-import React, { useState } from 'react'
-import { useFormik } from 'formik'
-import * as Yup from 'yup'
+import React from 'react'
+import { Formik, Form, Field, ErrorMessage } from 'formik'
 
 // import Button from '../Button'
+import { loginSchema } from '../../utils/validationsSchemas'
 import sprite from '../../assets/svg/sprite.svg'
 import s from './LoginForm.module.css'
 
-const loginSchema = Yup.object({
-  email: Yup.string().email('Invalid email').required('Required'),
-  password: Yup.string()
-    .min(2, 'Пароль должен состоять из более 2 символов')
-    .max(12, 'Слишком длинный пароль')
-    .required(),
-})
-
-const initialValues = { email: '', password: '' }
-
-const onSubmit = (values) => {
-  console.log('values', values)
-}
-
 const LoginForm = () => {
-  const formik = useFormik({
-    initialValues,
-    onSubmit,
-    validationSchema: loginSchema,
-  })
+  const initialValues = { email: '', password: '' }
+
+  const onSubmit = (values, onSubmitProps) => {
+    console.log(values)
+
+    onSubmitProps.setSubmitting(false)
+    onSubmitProps.resetForm()
+  }
 
   return (
     <div className={s.container}>
@@ -35,51 +24,62 @@ const LoginForm = () => {
         </svg>
       </div>
 
-      <form className={s.form} onSubmit={formik.handleSubmit}>
-        <label className={s.label}>
-          <input
-            type="email"
-            name="email"
-            placeholder="E-mail"
-            className={s.input}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.email}
-          />
-          <svg width="24" height="24" className={s.inputIcon}>
-            <use href={`${sprite}#icon-email`}></use>
-          </svg>
-        </label>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={loginSchema}
+        onSubmit={onSubmit}
+        validateOnChange={false}
+      >
+        <Form className={s.form}>
+          <label className={s.label}>
+            <Field
+              type="email"
+              name="email"
+              placeholder="E-mail"
+              className={s.input}
+            />
 
-        {/* вывод ошибки */}
-        {formik.touched.email && formik.errors.email}
+            <svg width="24" height="24" className={s.inputIcon}>
+              <use href={`${sprite}#icon-email`}></use>
+            </svg>
 
-        <label className={s.label}>
-          <input
-            type="password"
-            name="password"
-            placeholder="Пароль"
-            className={s.input}
-            onChange={formik.handleChange}
-            onBlur={formik.handleBlur}
-            value={formik.values.password}
-          />
-          <svg width="24" height="24" className={s.inputIcon}>
-            <use href={`${sprite}#icon-password`}></use>
-          </svg>
-        </label>
+            {/* вывод ошибки */}
+            <ErrorMessage
+              name="email"
+              component="p"
+              className={s.validErrorMes}
+            />
+          </label>
 
-        {/* вывод ошибки */}
-        {formik.touched.password && formik.errors.password}
+          <label className={s.label}>
+            <Field
+              type="password"
+              name="password"
+              placeholder="Пароль"
+              className={s.input}
+            />
 
-        <button className={s.logBtn} type="submit">
-          Вход
-        </button>
+            <svg width="24" height="24" className={s.inputIcon}>
+              <use href={`${sprite}#icon-password`}></use>
+            </svg>
 
-        <button type="button" className={s.regBtn}>
-          Регистрация
-        </button>
-      </form>
+            {/* вывод ошибки */}
+            <ErrorMessage
+              name="password"
+              component="p"
+              className={s.validErrorMes}
+            />
+          </label>
+
+          <button className={s.logBtn} type="submit">
+            Вход
+          </button>
+
+          <button type="button" className={s.regBtn}>
+            Регистрация
+          </button>
+        </Form>
+      </Formik>
     </div>
   )
 }
