5eef777d3f9a9f53df11f322e0e4c56818b4a199
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-05-18 20:18:38 +0300

    get user name from redux

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
+import { useSelector } from 'react-redux'
+import { getUser } from '../../redux/session/session-selectors'
 import s from './Header.module.css'
 import sprite from '../../assets/svg/sprite.svg'
 
 export default function Header() {
+  const { name } = useSelector(getUser)
   return (
     <>
       <header className={s.header}>
@@ -13,7 +16,7 @@ export default function Header() {
           </a>
         </div>
         <div className={s.authContainer}>
-          <p className={s.name}>Имя</p>
+          <p className={s.name}>{name ?? 'Имя'}</p>
           <svg className={s.lineSvg}>
             <use href={`${sprite}#icon-vertical-line`}></use>
           </svg>
