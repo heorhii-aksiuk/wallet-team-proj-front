21af23c249f7e27b377c59f10b259fc5e10002d3
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2022-05-19 13:09:53 +0300

    refactoring logo link and base config for exit button

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,3 +1,4 @@
+import { NavLink } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import { getUser } from '../../redux/session/session-selectors'
 import s from './Header.module.css'
@@ -5,27 +6,30 @@ import sprite from '../../assets/svg/sprite.svg'
 
 export default function Header() {
   const { name } = useSelector(getUser)
+  const handleClick = () => console.log('exit click') // Подключить модальное окно
+
   return (
     <>
       <header className={s.header}>
         <div className={s.logoContainer}>
-          <a href="./">
+          <NavLink exact to="./">
+            {/* Подставить путь главной страницы */}
             <svg className={s.logoSvg}>
               <use href={`${sprite}#icon-logo-full`}></use>
             </svg>
-          </a>
+          </NavLink>
         </div>
         <div className={s.authContainer}>
           <p className={s.name}>{name ?? 'Имя'}</p>
           <svg className={s.lineSvg}>
             <use href={`${sprite}#icon-vertical-line`}></use>
           </svg>
-          <a href="./">
+          <button className={s.button} onClick={handleClick} type="button">
             <svg className={s.exitSvg}>
               <use href={`${sprite}#icon-exit`}></use>
             </svg>
             <p className={s.exit}>Выйти</p>
-          </a>
+          </button>
         </div>
       </header>
     </>
